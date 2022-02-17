27fea50292b3a172abbb96f119838c631c8484d4
Monge <luizfilipemonge@outlook.com>
2022-02-17 14:18:45 -0300

    [Base] - Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+
+import Popper from '@popperjs/core';
+
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a class="btn btn-primary text-white" href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
     </div>
